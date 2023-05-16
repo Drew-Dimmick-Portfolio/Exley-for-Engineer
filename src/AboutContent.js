86f9c9292da1d507a273f5502973fdbf23020bbc
Drew-Dimmick-Portfolio <drew.dimmick1@gmail.com>
2023-05-16 17:12:21 -0400

    Fixed some typos in the about page

--- src/AboutContent.js
+++ src/AboutContent.js
@@ -18,11 +18,11 @@ export default function AboutContent() {
                         
                         <div className="bioText">
                             <p>State Licensed Professional Engineer and Surveyor.
-                            Experienced and Proactive problem solver</p>
+                            Experienced and Proactive problem solver.</p>
                         
                             <p>37 years married to wife Gayle, a retired pharmacist
                             2 adult sons, Adam and Jason, raised in Lake County.
-                            Adam and Jason have chosen to make Lake County their home</p>   
+                            Adam and Jason have chosen to make Lake County their home.</p>   
 
                             <p>12 years in the private sector managing large industrial infrastructure
                             projects throughout the nation developing skills of learned structure
@@ -33,10 +33,10 @@ export default function AboutContent() {
                             time to inspect, evaluate and report on the infrastructure
                             responsibilities of the Lake County Engineer.
                             Proactive on safety, programming projects and securing key State and
-                            Federal Funding for the projects built here in Lake County</p>
+                            Federal Funding for the projects built here in Lake County.</p>
 
                             <p>I believe when making a decision, listen first, then ask “What is in the
-                            best interest of Lake County?”.</p>
+                            best interest of Lake County?”</p>
                         </div>
                     </div>
                 </div>
